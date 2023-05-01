f9ad90612cd02f51a0c8a305a6d00aefb992640d
toddmath <tmatheson11186@gmail.com>
2023-05-01 11:07:28 -0700

    add data-structures workspace crate, move algos into it

--- data-structures/src/lib.rs
+++ data-structures/src/lib.rs
@@ -1,4 +1,4 @@
-//! Common algorithms implemented in rust
+//! Common data structures.
 
 #![warn(
     missing_docs,
@@ -14,3 +14,16 @@
     unused_import_braces,
     unused_qualifications
 )]
+
+mod avl_tree;
+mod b_tree;
+mod heap;
+mod linked_list;
+mod queue;
+mod stack_using_singly_linked_list;
+mod trie;
+
+pub use self::{
+    avl_tree::AVLTree, b_tree::BTree, heap::Heap, linked_list::LinkedList, queue::Queue,
+    stack_using_singly_linked_list::Stack, trie::Trie,
+};
