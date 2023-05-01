4ac374d76b3efebb553ee86d6cb75fc73e7721d8
toddmath <tmatheson11186@gmail.com>
2023-04-30 19:50:33 -0700

    move math algorithms to math workspace crate

--- math/src/lib.rs
+++ math/src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+//! Common math algorithms
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod abs;
+mod aliquot_sum;
+mod collatz_sequence;
+mod factors;
+mod fast_fourier_transform;
+mod faster_perfect_numbers;
+mod gaussian_elimination;
+mod lcm_of_n_number;
+mod linear_sieve;
+mod matrix_ops;
+mod mersenne_primes;
+mod prime_numbers;
+mod random;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use self::{
+    abs::abs,
+    aliquot_sum::aliquot_sum,
+    collatz_sequence::sequence,
+    factors::factors,
+    fast_fourier_transform::*,
+    faster_perfect_numbers::generate_perfect_numbers,
+    gaussian_elimination::gaussian_elimination,
+    lcm_of_n_number::lcm,
+    linear_sieve::LinearSieve,
+    matrix_ops::*,
+    mersenne_primes::{get_mersenne_primes, is_mersenne_prime},
+    prime_numbers::prime_numbers,
+    random::PCG32,
+};
